6001741a29f39cd073d9a5e3eab1e4332a1b3f7b
ahmedadelattya <ahmedadelattya2@gmail.com>
2024-09-05 22:16:30 +0300

    creating job table and make the relation between jobs and employers

---
+++ database/migrations/2024_09_05_142735_create_job_listings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_listings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employer_id')->constrained('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('title');
+            $table->text('description');
+            $table->string('category');
+            $table->string('location');
+            $table->enum('salary_type', ['fixed', 'hourly']);
+            $table->decimal('fixed_salary', 10, 2)->nullable();
+            $table->decimal('hourly_rate', 10, 2)->nullable();
+            $table->json('skills')->nullable();
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_listings');
+    }
+};
