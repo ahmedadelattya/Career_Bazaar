cb43060ef2f436df5c875c192f38e118fddd56cf
MohamedSalahAbdallah <eng.mohamedsalahbakr@gmail.com>
2024-09-07 02:14:48 +0300

    add admin dashboard and controller

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function getAllJobs(Request $request)
+    {
+        return;
+    }
+}
--- resources/views/admin-dashboard.blade.php
+++ resources/views/admin-dashboard.blade.php
@@ -1,17 +1,20 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-zinc-800 dark:text-zinc-200 leading-tight">
+        <h2 class="text-xl font-semibold leading-tight text-zinc-800 dark:text-zinc-200">
             {{ __('Admin_DashBoard') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-zinc-800 overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm dark:bg-zinc-800 sm:rounded-lg">
                 <div class="p-6 text-zinc-900 dark:text-zinc-100">
                     {{ __("You're logged in!") }}
                 </div>
             </div>
         </div>
     </div>
+
+    <!-- Admin Dashboard Content -->
+    
 </x-app-layout>
