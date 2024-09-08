cdcc632a68ca4426cbba41c72bf0563d525d96b2
MohamedSalahAbdallah <eng.mohamedsalahbakr@gmail.com>
2024-09-08 20:23:18 +0300

    added admin functionality, ui and added the routes to web.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,13 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Job;
 use App\Models\User;
+use App\Notifications\JobStatusNotification;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function getAllJobs(Request $request)
     {
-        return;
+        return view('admin-dashboard', ['jobs' => Job::get()->where('status', '=', 'pending')]);
+    }
+
+    public function approveJob($jobId)
+    {
+        $job = Job::find($jobId);
+        $job->status = 'approved';
+        $job->save();
+        $admin = User::where('id', '=', Auth::user()->id)->first();
+        $admin->notify(new JobStatusNotification($job, 'approved'));
+
+        return redirect()->back()->with('success', 'Job approved and admin notified.');
+        return redirect()->back();
+    }
+
+    public function rejectJob($jobId)
+    {
+        $job = Job::find($jobId);
+        $job->status = 'declined';
+        $job->save();
+        $admin = User::where('id', '=', Auth::user()->id)->first();
+        $admin->notify(new JobStatusNotification($job, 'declined'));
+
+        return redirect()->back()->with('success', 'Job rejected and admin notified.');
+
+        return redirect()->back();
     }
 }
